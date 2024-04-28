cc16eb10817820f01dc2c6a121e98a745d4d4263
Shaleen-Ndirangu <ndirangu.shaleen@gmail.com>
2024-04-28 18:20:15 +0300

    commit

--- load_balancer.py
+++ load_balancer.py
@@ -1,11 +1,12 @@
 import asyncio
 
 class LoadBalancer:
-    def __init__(self, servers):
+    def __init__(self, servers):  # Initialize the load balancer
         self.servers = servers
         self.server_index = 0
 
     async def handle_request(self, request):
+          # Handle incoming requests by distributing them among servers
         server = self.servers[self.server_index]
         self.server_index = (self.server_index + 1) % len(self.servers)
         response = await self.send_request(server, request)
@@ -17,10 +18,12 @@ class LoadBalancer:
         return f"Response from {server}: {request}"
 
 async def main():
+     # Define list of servers
     servers = ["Server1", "Server2", "Server3"]
     load_balancer = LoadBalancer(servers)
+    # Define list of requests
     requests = ["Request1", "Request2", "Request3", "Request4"]
-
+ # Send requests to load balancer and gather responses asynchronously
     responses = await asyncio.gather(*[load_balancer.handle_request(request) for request in requests])
     for response in responses:
         print(response)
